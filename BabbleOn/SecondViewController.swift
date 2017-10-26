49be34359eaa78095091ab2270e74548b0a20b6f
Dan Lindsay <dlindsay72@hotmail.com>
2017-10-26 13:46:41 -0400

    Initial Commit

---
+++ BabbleOn/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BabbleOn
+//
+//  Created by Dan Lindsay on 2017-10-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
