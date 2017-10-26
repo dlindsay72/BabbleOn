e2d4b9bcd812651fdafab4094d18ea1c2ee8ad3c
Dan Lindsay <dlindsay72@hotmail.com>
2017-10-26 16:50:39 -0400

    ui for AuthVC and LoginVC done

---
+++ BabbleOn/Controller/AuthVC.swift
@@ -0,0 +1,21 @@
+//
+//  AuthVC.swift
+//  BabbleOn
+//
+//  Created by Dan Lindsay on 2017-10-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
---
+++ BabbleOn/Controller/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  BabbleOn
+//
+//  Created by Dan Lindsay on 2017-10-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
